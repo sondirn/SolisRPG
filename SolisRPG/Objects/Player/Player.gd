266eae20edd3d6f0bb205925a22de870ac1ce487
Miguel <sondirn@gmail.com>
2020-04-01 11:53:55 -0500

    lkj

--- SolisRPG/Objects/Player/Player.gd
+++ SolisRPG/Objects/Player/Player.gd
@@ -8,7 +8,7 @@ export var ACCELERATION: float = 1200
 export var FRICTION: float = 1200
 export var MAX_SPEED: float = 100
 
-signal destination_reached
+#signal destination_reached
 onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 onready var animationTree: AnimationTree = $AnimationTree
 onready var animationState: AnimationNodeStateMachinePlayback = animationTree.get("parameters/playback")
@@ -29,41 +29,28 @@ var _speed_modifier: float = 1
 func _ready():
 	Fsm = get_node("StateMachine")
 	Fsm._prepare()
+	destination = position
 	
 
 func _process(_delta):
 	#velocity = Vector2.ZERO
-	var move_distance:float = MAX_SPEED * _speed_modifier
-	var velocity = (destination - position).normalized() * move_distance
-	if (destination - position).length() > 5:
-		velocity = move_and_slide(velocity)
-	pass
-	
-func generate_path(destinationPoint: Vector2):
-	
-	var closest_destination = nav_2d.get_closest_point(destinationPoint)
-	var new_path := nav_2d.get_simple_path(position, closest_destination)
-	path = new_path
-		
-
-func move_in_path(move_distance: float) -> void:
-	var starting_point : Vector2 = position
-	for point in range(path.size()):
-		var distance_to_next_point = starting_point.distance_to(path[0])
-		if move_distance <= distance_to_next_point:
-			var move_rotation = get_angle_to(starting_point.linear_interpolate(path[0], move_distance / distance_to_next_point))
-			velocity = Vector2(MAX_SPEED * _speed_modifier, 0).rotated(move_rotation)
-			move_and_slide(velocity)
-			break
-		move_distance -= distance_to_next_point
-		starting_point = path[0]
-		path.remove(0)
-	if path.size() == 0:
-		emit_signal("destination_reached")
-
-func _input(event):
+	var move_distance:float = MAX_SPEED * _speed_modifier * _delta
 	if Input.is_action_pressed("click"):
 		destination = get_global_mouse_position()
+	var starting_point : Vector2 = position
+	var distance_to_destination = starting_point.distance_to(destination)
+	var collision_check = starting_point.direction_to(destination)
+	if move_distance <= distance_to_destination:
+		var move_rotation = get_angle_to(starting_point.linear_interpolate(destination, move_distance / distance_to_destination))
+		var velocity = Vector2(MAX_SPEED * _speed_modifier, 0).rotated(move_rotation)
+		var collision = move_and_collide(collision_check * _delta)
+		velocity = move_and_slide(velocity)
+		if collision != null:
+			destination = position
+		
+
+
+func _input(event):
 	if Input.is_action_pressed("shift_modifier") && event.is_action_pressed("cast_spell"):
 		var fireball = load ("res://Objects/Spells/FireBall.tscn")
 		var fireball_instance = fireball.instance()
@@ -72,3 +59,5 @@ func _input(event):
 		get_parent().add_child(fireball_instance)
 		fireball_instance = null
 		fireball = null
+
+
