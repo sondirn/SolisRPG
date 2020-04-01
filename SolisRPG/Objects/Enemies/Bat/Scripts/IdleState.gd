8cd267fa6547b26acc2f09bc3370ca22747b5449
Miguel Guzman <sondirn@gmail.com>
2020-04-01 18:01:20 -0500

    test

--- SolisRPG/Objects/Enemies/Bat/Scripts/IdleState.gd
+++ SolisRPG/Objects/Enemies/Bat/Scripts/IdleState.gd
@@ -1,15 +1,39 @@
 extends State
 
+onready var spawn_point: Vector2
 
 var Fsm: StateMachine
-var Parent: Node2D
+var Parent: Enemy
+var kinematic_body: KinematicBody2D
 
 func _prepare():
 	Fsm = get_parent()
-	Parent = Fsm.get_parent()
+	Parent = Fsm.get_parent() as Enemy
+	kinematic_body = Parent as KinematicBody2D
+	spawn_point = kinematic_body.position
 
 func _on_enter():
-	var anim : AnimationTree = Parent.get_node("AnimationTree")
-	var state : AnimationNodeStateMachinePlayback = anim.get("parameters/playback")
-	state.travel("Idle")
+	generate_destination()
+
+func _update(_delta):
+	var move_distance:float = Parent.max_speed * Parent.speed_modifier * _delta
+	var starting_point: Vector2 = Parent.position
+	var distance_to_destination = starting_point.distance_to(Parent.destination)
+	if distance_to_destination == 0:
+		return
+	if move_distance <= distance_to_destination:
+		var move_rotation = Parent.get_angle_to(starting_point.linear_interpolate(Parent.destination, move_distance / distance_to_destination))
+		var velocity = Vector2(Parent.max_speed * Parent.speed_modifier, 0).rotated(move_rotation)
+		velocity = Parent.move_and_slide(velocity)
+	else:
+		generate_destination()
+		
+func generate_destination() -> void:
+	#yield(get_tree().create_timer(3.0), "timeout")
+	test()
+func test() -> void:
+	var rand_rotation = rand_range(0, 259)
+	var test_dest = Vector2(Parent.search_range, 0).rotated(rand_rotation)
+	print(test_dest)
+	Parent.destination = spawn_point + test_dest
 	
---
+++ SolisRPG/Objects/Enemies/Enemy.gd
@@ -0,0 +1,30 @@
+class_name Enemy
+extends KinematicBody2D
+
+
+var speed_modifier: float = 1.0
+export var search_range:float
+export var health: float
+export var max_speed: float
+
+var path:= PoolVector2Array()
+var destination:= Vector2() setget set_destination
+
+#nodes
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+onready var animationTree: AnimationTree = $AnimationTree
+onready var animationState: AnimationNodeStateMachinePlayback = animationTree.get("parameters/playback")
+onready var sprite: Sprite = $Sprite
+onready var scene: Node2D = get_parent().get_parent().get_parent()
+onready var nav_2d: Navigation2D = scene.get_node("Navigation2D")
+
+var Fsm: StateMachine
+
+func _ready():
+	Fsm = get_node("StateMachine") as StateMachine
+	Fsm._prepare()
+
+
+func set_destination(value: Vector2):
+	destination = value
+
